15bb755fc934ea204e6e05be8aace7399236e91c
Lee Speed <lee-speed@live.co.uk>
2018-10-15 11:29:03 +0100

    Update

---
+++ Advanced Games and Dev/Assets/Scripts/Lobby/Room/CurrentRoomCanvas.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CurrentRoomCanvas : MonoBehaviour {
+
+    public void OnClickStartSync()
+    {
+        PhotonNetwork.LoadLevel(1);
+    }
+
+    public void OnClickStartDelayed()
+    {
+        PhotonNetwork.room.IsOpen = false;
+        PhotonNetwork.room.IsVisible = false;
+        PhotonNetwork.LoadLevel(1);
+    }
+
+}
---
+++ Advanced Games and Dev/Assets/Scripts/Lobby/UI/mainMenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainMenuManager : MonoBehaviour {
+
+    public GameObject createGame, joinGame, lobbyView2;
+
+	public void OnCreate()
+    {
+        createGame.SetActive(true);
+        lobbyView2.SetActive(false);
+    }
+
+    public void OnJoin()
+    {
+        joinGame.SetActive(true);
+        lobbyView2.SetActive(false);
+        joinGame.transform.SetAsLastSibling();
+    }
+
+}
